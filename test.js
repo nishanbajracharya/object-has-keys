0e323826197f076068d9d6c679ddd58f8ae10c90
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-05-13 20:24:07 +0545

    Update tests

--- test.js
+++ test.js
@@ -3,11 +3,27 @@ const expect = require('chai').expect;
 const hasKeys = require('./index');
 
 describe('Simple Positive Test', () => {
-  it('should return true', () => {
+  it('should return true for single string key', () => {
     const object = { key: 'value' };
 
     const result = hasKeys(object, 'key');
 
     expect(result).to.equal(true);
-  })
+  });
+
+  it('should return true for array of single key', () => {
+    const object = { key: 'value' };
+
+    const result = hasKeys(object, ['key']);
+
+    expect(result).to.equal(true);
+  });
+
+  it('should return true for array of keys', () => {
+    const object = { key1: 'value', key2: 'value' };
+
+    const result = hasKeys(object, ['key1', 'key2']);
+
+    expect(result).to.equal(true);
+  });
 });
